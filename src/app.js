9c4c3fe0d448cb0c46ea00978fe3af4ecfc66b0c
Anuj Mumbaikar <anujmumbaikar@Anuj-Macbook.local>
2025-02-25 00:57:22 +0530

    router and controller

--- src/app.js
+++ src/app.js
@@ -12,5 +12,18 @@ app.use(express.urlencoded({extended:true , limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes import ..... most of the code bases pe routes yaha pe hi import karte hai.
+//upar bhi kr sakte hai
+import userRouter from "./routes/user.routes.js"
+//routes declaration(below)
+//earlier we used to write app.get but now things are sperated in diff files 
+//now we want middleware also to take router  hence we use app.use
+
+app.use("/api/v1/users",userRouter) //it will works as http://localhost:8000/api/v1/users/register
+//now if we want login also then we dont hv to call it once again. we add directly in userRouter
+
+
+
+
  
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // require('dotenv').config({path:'./env'})
 //or
+import {app} from "./app.js"
 import dotenv from "dotenv"
 import connectDB from './db/index.js';
 dotenv.config({
